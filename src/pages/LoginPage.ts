7d9a254a42e1b817a9b816c1d80ea6a8f482dec7
HoangBV <hoangga714@gmail.com>
2025-12-04 00:24:11 +0700

    feat: add LoginPage class with user credentials locators

---
+++ src/pages/LoginPage.ts
@@ -0,0 +1,29 @@
+import { Page, expect} from "@playwright/test";
+import BasePage from "./BasePage";
+
+export default class LoginPage extends BasePage{
+    private readonly xpathUserName = "#username";
+    private readonly xpathPassword = "#password";
+    private readonly xpathBtnLogin = "#Login";
+    
+
+
+    constructor(page : Page){
+        super(page);
+    }
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
